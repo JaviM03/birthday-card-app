582791184d906590010971417f00330a11064c77
EdwinM96 <edwin.morales9b@gmail.com>
2020-01-13 21:17:56 -0600

    Added Friend domain class

--- src/main/java/com/christmascards/domain/Friend.java
+++ src/main/java/com/christmascards/domain/Friend.java
@@ -5,10 +5,121 @@
  */
 package com.christmascards.domain;
 
+import java.util.Calendar;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
 /**
  *
  * @author HP PC
  */
+
+@Entity
+@Table(schema = "public",name="refered_friend")
 public class Friend {
+    
+    @Id
+    @GeneratedValue
+    @Column(name="user_id")
+    private Integer userId;
+    
+    @Column(name="username")
+    private String username;
+    
+    @Column(name="user_password")
+    private String userPassword;
+    
+    @Column(name="email")
+    private String email;
+    
+    @Column(name="phone_number")
+    private String phoneNumber;
+    
+    @Column(name="first_name")
+    private String firstName;
+    
+    @Column(name="last_name")
+    private String lastName;
+    
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(iso = ISO.DATE)
+    @Column(name="birth_date")
+    private Calendar birthDate;
+
+    public Friend() {
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Calendar getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Calendar birthDate) {
+        this.birthDate = birthDate;
+    }
+    
+    
+    
     
 }
