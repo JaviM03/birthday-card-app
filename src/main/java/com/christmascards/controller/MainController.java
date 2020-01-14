ae7604dd72831823c03e45c8c2bb801371fda7f1
EdwinM96 <edwin.morales9b@gmail.com>
2020-01-13 19:54:23 -0600

    Added correct dependencies and configuration files

---
+++ src/main/java/com/christmascards/controller/MainController.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.christmascards.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ *
+ * @author HP PC
+ */
+@Controller
+public class MainController {
+    
+    @RequestMapping(value="/")
+    public ModelAndView index(){
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("index");
+        return mv;
+    }
+    
+}
