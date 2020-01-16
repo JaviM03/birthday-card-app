2eef03befc55247bc0c66be6b7855657f261e227
EdwinM96 <edwin.morales9b@gmail.com>
2020-01-16 10:07:09 -0600

    Added reference to domain's generated id's

--- src/main/java/com/christmascards/domain/User.java
+++ src/main/java/com/christmascards/domain/User.java
@@ -9,6 +9,7 @@ import java.util.Calendar;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -25,7 +26,7 @@ import org.springframework.format.annotation.DateTimeFormat.ISO;
 public class User {
     
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     @Column(name="user_id")
     private Integer userId;
     
