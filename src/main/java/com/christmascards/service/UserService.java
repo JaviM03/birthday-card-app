ac03ecd89491e589cb17f348ce87c41952a1f491
EdwinM96 <edwin.morales9b@gmail.com>
2020-01-20 10:23:46 -0600

    Added check non-repeat email and phone method

--- src/main/java/com/christmascards/service/UserService.java
+++ src/main/java/com/christmascards/service/UserService.java
@@ -9,6 +9,7 @@ import com.christmascards.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.christmascards.domain.User;
+import java.util.List;
 import org.springframework.transaction.annotation.Transactional;
 /**
  *
@@ -22,8 +23,36 @@ public class UserService {
     UserRepository userRepo;
     
     @Transactional
-    public User saveUser(User user){
+    public User registerUser(User user){
         return userRepo.saveAndFlush(user);
+    }
+    
+    //Checks if phone number and email have already beign used, which if true would mean that the user alreeady has an account
+    //"none" means no results were found for both, "both" means both email and phone number were found on an already existing account
+    //"phone" means phone number was found on an already existing account, "email" means email was found on an already existing account
+    @Transactional
+    public String checkUserEmailAndPhone(String email, String phoneNumber){
+        List<User> searchByEmail = userRepo.findByEmail(email);
+        List<User> searchByPhone = userRepo.findByPhoneNumber(phoneNumber);
+        String response = "none";
+        
+        
+        if(searchByEmail!=null){
+            if(!searchByEmail.isEmpty()){
+                response = "email";
+            }
+        }
+        if(searchByPhone!=null){
+            if(!searchByPhone.isEmpty()){
+                if(response.equals("email")){
+                    response="phone";
+                }
+                else{
+                    response="both";
+                }
+            }
+        }
+        return response;
     }
     
 }
