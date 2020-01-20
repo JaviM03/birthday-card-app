9d3f622009b3e609442c358c56f3b28e2b8c711b
EdwinM96 <edwin.morales9b@gmail.com>
2020-01-20 10:40:28 -0600

    Changed Twilio dependency to another version

--- src/main/java/com/christmascards/controller/MainController.java
+++ src/main/java/com/christmascards/controller/MainController.java
@@ -5,8 +5,13 @@
  */
 package com.christmascards.controller;
 
+import com.christmascards.domain.User;
+import java.util.logging.Logger;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -16,16 +21,18 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class MainController {
     
+    Logger l = Logger.getLogger("logger");
+    
     @RequestMapping(value="/")
     public ModelAndView index(){
         ModelAndView mv = new ModelAndView();
         mv.setViewName("index");
         return mv;
-    }
+    }   
     
-    @RequestMapping(value="/register")
-    public ModelAndView register(){
-        ModelAndView mv = new ModelAndView("register");
+    @RequestMapping(value="/login")
+    public ModelAndView login(){
+        ModelAndView mv = new ModelAndView("login");
         return mv;
     }
     
